6164833f107c24d50ac315d39cf63458c4e6f1f0
easttiger <easttiger@gmail.com>
2016-02-13 02:17:30 +0800

    update simulation script

--- simulation/simulation.R
+++ simulation/simulation.R
@@ -1,70 +1,124 @@
 #configure parallel mode
-options(run_parallel=T)
+options(run_parallel = T)
 NCORES = as.integer(Sys.getenv("NUMBER_OF_PROCESSORS"))
 require("snow")
 require("imultinom")
-p = t(matrix(c(0.1654,0.2024,0.1444,0.1532,0.2301,0.1046),nrow=3))
+p = t(matrix(c(
+  0.1654,0.2024,0.1444,0.1532,0.2301,0.1046
+),nrow = 3))
 subsize = c(120,40,40,100)
-De = matrix(c(1,1,1,0,0,0,0,0,0,1,1,1,1,0,0,1,0,0,0,1,0,0,1,0,0,0,1,0,0,1),nrow=6)
+De = matrix(c(
+  1,1,1,0,0,0,0,0,0,1,1,1,1,0,0,1,0,0,0,1,0,0,1,0,0,0,1,0,0,1
+),nrow = 6)
 
 sim_concreteExampleFixedPartition <-
-function(samplesize=1){
-  # this block simulates from 1.2 A concrete example
-  # fix: partition and sample sizes
-  res = NULL
-  im = imultinom(a=double(nrow(De)),b=double(ncol(De)),De=De)
+  function(samplesize = 1) {
+    # this block simulates from 1.2 A concrete example
+    # fix: partition and sample sizes
+    res = NULL
+    im = imultinom(a = double(nrow(De)),b = double(ncol(De)),De = De)
 
-  cl <- makeSOCKcluster(rep("localhost",NCORES))
-  clusterExport(cl, list("p","subsize","De","im","mle"),envir=environment())
-  res = parLapply(cl, 1:samplesize, function(x){
-    y = list()
-    y[[1]] = as.vector(table(sample(x=c(1,4,2,5,3,6),prob=p,size=subsize[1],replace=T)))
-    y[[2]] = as.vector(table(sample(x=c("135","246"),prob=apply(p,1,sum),size=subsize[2],replace=T)))
-    y[[3]] = as.vector(table(sample(x=c("14","25","36"),prob=apply(p,2,sum),size=subsize[3],replace=T)))
-    y[[3]][1] = y[[3]][1] - 100
-    y[[4]] = as.vector(table(sample(x=c("1","4"),prob=p[,1],size=subsize[4],replace=T)))
-    a = y[[1]]
-    a[1] = a[1] + y[[4]][1]
-    a[4] = a[4] + y[[4]][2]
-    b = c(y[[2]], y[[3]])
-    im@a = a;
-    im@b = b;
-    mle = mle(im)
-    obsInfoMat = solve(mle$covmle[1:5,1:5])
-    phat = mle$x
-    covphat = mle$covmle
-    list( phat = phat,
-          covphat = covphat,
-          sdphat = sqrt(as.vector(diag(covphat))),
-          chi2 = as.double(t(t(p)[-6] - phat[-6]) %*% obsInfoMat %*% (t(p)[-6] - phat[-6])))
-  })
-  stopCluster(cl)
-  save(res,file=paste("c:/temp/sim_concreteExampleFixedPartition(",samplesize,")",format(Sys.time(),"_%Y-%m-%d_%H.%M.%S.rda"),sep=""))
-  res
-}
+    cl <- makeSOCKcluster(rep("localhost",NCORES))
+    clusterExport(cl, list("p","subsize","De","im","mle"),envir = environment())
+    res = parLapply(cl, 1:samplesize, function(x) {
+      y = list()
+      y[[1]] = as.vector(table(sample(
+        x = c(1,4,2,5,3,6),prob = p,size = subsize[1],replace = T
+      )))
+      y[[2]] = as.vector(table(sample(
+        x = c("135","246"),prob = apply(p,1,sum),size = subsize[2],replace = T
+      )))
+      y[[3]] = as.vector(table(sample(
+        x = c("14","25","36"),prob = apply(p,2,sum),size = subsize[3],replace =
+          T
+      )))
+      y[[3]][1] = y[[3]][1] - 100
+      y[[4]] = as.vector(table(sample(
+        x = c("1","4"),prob = p[,1],size = subsize[4],replace = T
+      )))
+      a = y[[1]]
+      a[1] = a[1] + y[[4]][1]
+      a[4] = a[4] + y[[4]][2]
+      b = c(y[[2]], y[[3]])
+      im@a = a;
+      im@b = b;
+      mle = mle(im)
+      obsInfoMat = solve(mle$covmle[1:5,1:5])
+      phat = mle$x
+      covphat = mle$covmle
+      list(
+        phat = phat,
+        covphat = covphat,
+        sdphat = sqrt(as.vector(diag(covphat))),
+        chi2 = as.double(t(t(p)[-6] - phat[-6]) %*% obsInfoMat %*% (t(p)[-6] - phat[-6]))
+      )
+    })
+    stopCluster(cl)
+    save(
+      res,file = paste(
+        "c:/temp/sim_concreteExampleFixedPartition(",samplesize,")",format(Sys.time(),"_%Y-%m-%d_%H.%M.%S.rda"),sep =
+          ""
+      )
+    )
+    res
+  }
 
 coverage.probability <-
-function(n){ #best n=12000
-  proba = seq(1.0/n,1.0,1.0/n)
-  y=sim_concreteExampleFixedPartition(n)
-  phat = matrix(nrow=6,Reduce("c", sapply(y, function(x)(x$phat))))
-  write.csv(t(phat),format(Sys.time(),"c:/temp/phat_%Y-%m-%d_%H.%M.%S.csv"))
-  sdphat = matrix(nrow=6,Reduce("c", sapply(y, function(x)(x$sdphat))))
-  z = (phat-as.vector(t(p)))/sdphat
-  chi2=Reduce("c", sapply(y, function(x)x$chi2))
-  layout(matrix(c(1,1,1,1,1,1,2,3,4,5,6,7),nrow=4,ncol=3,byrow=T))
-  plot(proba,sapply(proba, function(x){sum(chi2 < qchisq(p=x,df=5)) / n}),main="Joint Coverage Probability (PP-Plot)", xlab="Cumulative Probabilities of Chisq(5) ", ylab="Empirical Cumulative Probabilities", type='p',cex=0.2)
-  abline(0,1)
-  for(i in 1:6){
-    x = phat[i,]
-    hist(x,prob=T,nclass=50,xlab=bquote(hat(p)[.(i)]),xaxt="n",main=paste("95% CI Coverage Prob.\n",(sum(abs(z[i,]) <= qnorm(p=0.975)))/n),cex.main=0.9)
-    axis(1,at=seq(0,0.4,0.01))
-    lines(seq(min(x),max(x),0.001),dnorm(seq(min(x),max(x),0.001),mean=t(p)[i],sd=mean(sdphat[i,])))
+  function(n) {
+    #best n=12000
+    proba = seq(1.0 / n,1.0,1.0 / n)
+    y = sim_concreteExampleFixedPartition(n)
+    phat = matrix(nrow = 6,Reduce("c", sapply(y, function(x)
+      (x$phat))))
+    write.csv(t(phat),format(Sys.time(),"c:/temp/phat_%Y-%m-%d_%H.%M.%S.csv"))
+    sdphat = matrix(nrow = 6,Reduce("c", sapply(y, function(x)
+      (x$sdphat))))
+    z = (phat - as.vector(t(p))) / sdphat
+    chi2 = Reduce("c", sapply(y, function(x)
+      x$chi2))
+    #layout(matrix(c(1,1,1,1,1,1,2,3,4,5,6,7,8,9,10,11,12,13),nrow=6,ncol=3,byrow=T))
+    par(mfrow = c(1,1))
+    plot(
+      proba,sapply(proba, function(x) {
+        sum(chi2 < qchisq(p = x,df = 5)) / n
+      }),main = "Joint Coverage Probability\n at all conficence levels", xlab =
+        "Cumulative Probabilities of Chisq(5) (The Confidence Level)", ylab = "Empirical Cumulative Probabilities (1 - pvalue)", type = 'p',cex = 0.2
+    )
+    abline(a=0,b=1,col=2)
+    par(mfrow = c(4,3))
+    par(cex.axis=0.8)
+    for (i in 1:6) {
+      plot(
+        proba,sapply(proba, function(x) {
+          sum(z[i,] < qnorm(p = x)) / n
+        }), main = bquote(paste(
+          "Marginal Coverage Pr. of ", p[.(i)], " all C.L.", sep = ""
+        )),cex.main = 0.9, xlab = paste(
+          "Cum. Pr. N(", format(100 * t(p)[i],digits = 4), "%, (", format(100 * mean(sdphat[i,]),digits = 3), "%)^2)", sep = ""
+        ),ylab = "Emp.Cum.Pr.",cex = 0.1
+      )
+      abline(a=0,b=1,col=2)
+    }
+    par(cex.axis=0.8)
+    for (i in 1:6) {
+      x = phat[i,]
+      hist(
+        x,prob = T,nclass = 50, xlab=NULL,ylab = NULL,xaxt="n",main = bquote(paste("Marginal Emp. Dist. of ", hat(p)[.(i)])),cex.main =
+          0.9
+      ) #, bquote(hat(p)[.(i)])
+      sd = mean(sdphat[i,])
+      axis(1,at = seq(round(t(p)[i]-3.5*sd,2),round(t(p)[i]+3.5*sd,2),length.out=5
+      ))
+      lines(seq(min(x),max(x),0.001),dnorm(
+        seq(min(x),max(x),0.001),mean = t(p)[i],sd = mean(sdphat[i,])
+      ),col = 2)
+    }
   }
-}
 
 f1 <-
-function(){
-  tryCatch(y=sim_concreteExampleFixedPartition(60000), error = function(e) f1())
-}
-
+  function() {
+    tryCatch(
+      y = sim_concreteExampleFixedPartition(60000), error = function(e)
+        f1()
+    )
+  }
--- simulation/simulation2.R
+++ simulation/simulation2.R
@@ -1,5 +1,5 @@
 # setwd("E:/Dropbox/Subjects/W/Weaver Paper Reading list/Real Data Examples")
-source("weaver.R")
+library(imultinom)
 RUN_PARALLEL = T
 library(snow)
 m=1;
@@ -13,7 +13,7 @@ randparti <-
     #m_range = seq(from= 10 * d, to= 100 * d, by= 1L)
     m_range = seq(from= m * d, to= 2 * m * d, by= 1L)
     nu = sample(x = m_range, size = 1L)
-    
+
     #draw partition
     #draw an upper bound of the number of subsets in the partition
     nSubsets = sample(x = 2:d, size=1L)
@@ -22,19 +22,19 @@ randparti <-
     #include 0 for the simulation of conditioning
     mSubsets = sample(x = 0:nSubsets, size=d, replace=T)
     uSubsets = unique(sort(mSubsets))
-    nSubsets = length(uSubsets) # if 0 is included in the outcome, 
-    # there will be an additional negative-exponent subset, 
+    nSubsets = length(uSubsets) # if 0 is included in the outcome,
+    # there will be an additional negative-exponent subset,
     # but that will not break this expression
-    
+
     #if(RUN_PARALLEL){
     #cl <- makeSOCKcluster(rep("localhost",ncluster))
     #clusterExport(cl, list("p","mSubsets","d"))
     #p_Xi = parSapply(cl, uSubsets,function(x){ifelse(x==0,0,sum(p[which(mSubsets == x)]))})
-    
+
     #}else{
     p_Xi = sapply(uSubsets,function(x){ifelse(x==0,0,sum(p[which(mSubsets == x)]))})
     #}
-    
+
     #if(RUN_PARALLEL){
     #  Xi = parSapply(cl,uSubsets,function(x){
     #    res=rep(0,d);
@@ -52,7 +52,7 @@ randparti <-
       res})
     #}
     y = as.vector(rmultinom(1,nu,p_Xi))
-    
+
     if(uSubsets[1] == 0){
       #need to modify the first col of Xi and first element of b
       Xi[,1] = apply(Xi,1,sum)
@@ -61,7 +61,7 @@ randparti <-
     list(y=y,Xi=Xi,nu=nu)
   }
 
-g <-  
+g <-
   function(p,R,includeCompleteData = TRUE){
     #if(RUN_PARALLEL) cl <- makeSOCKcluster(c("localhost","localhost"))
     p = p / sum(p)
@@ -81,21 +81,21 @@ g <-
     Xi = matrix(Xi, nrow = d)
     #index of a
     colsum = as.vector(apply(Xi,2,sum))
-    
+
     idx_b = which((colsum > 1) & (colsum < d))
     b = y[idx_b]
     De = Xi[,idx_b]
-    
+
     if(includeCompleteData){
       a = as.vector(apply(rmultinom(R, nu, p), 1, sum)) #initialise a with prior thickness
     }else{
       a = double(d) + 0.00001
     }
     if(length(y[colsum==1]) > 1){
-      a = a + as.vector(Xi[,colsum == 1] %*% y[colsum == 1]); 
+      a = a + as.vector(Xi[,colsum == 1] %*% y[colsum == 1]);
     }
     if(length(y[colsum==1]) == 1){
-      a = a + as.vector(Xi[,colsum == 1] * y[colsum == 1]); 
+      a = a + as.vector(Xi[,colsum == 1] * y[colsum == 1]);
     }
     #if(RUN_PARALLEL) stopCluster(cl)
     list(a=a,b=b,De=De)
@@ -108,7 +108,7 @@ mleSim <-
     stopifnot(all(p>0))
     d = length(p)
     p = p / sum(p)
-    
+
     #  res = matrix(double(d * samplesize), nrow=d)
     #  for(i in 1:samplesize){
     #    lik=g(p,R);
@@ -120,7 +120,7 @@ mleSim <-
       cl <- makeSOCKcluster(rep("localhost",ncluster))
       clusterExport(cl, list("p","d","R"), envir = sys.frame(sys.nframe()))
       clusterExport(cl, list("randparti","incComplete","Weaver","WeaverBayes","WeaverBas","WeaverGre","initWeaver","g","m"), envir = .GlobalEnv)
-      
+
       phat = parSapply(cl, 1:samplesize, function(x){
         lik=g(p,R,includeCompleteData = incComplete);
         if(m * 1.7^R >=d){
@@ -129,7 +129,7 @@ mleSim <-
           Weaver(lik$a,lik$b,t(lik$De))$x
         }
       })
-      
+
       stopCluster(cl)
     }else{
       phat = sapply(1:samplesize, function(x){
@@ -148,7 +148,7 @@ mleSim <-
     #boxplot(t(phat));
     #plot(p,apply(phat,1,var));
     #par(mfrow=oldmfrow)
-    
+
     phat
   }
 
@@ -165,7 +165,7 @@ function(){
   #boxplot(t(lastvar),names=seq(2,20,2),main=expression(bold(paste("Decreasing var(",hat(italic(p))[100],") as ", italic(R)," increases",sep=""))),ylab=expression(hat(italic(p))[100]))
   boxplot(t(lastvar),names=seq(2,20,2),main="",ylab=expression(hat(italic(p))[100]))
   title(xlab=expression(bold(R)),line=2)
-  
+
   phatmat = matrix(double(4*100),nrow=100)
   i=1
   for(R in c(2,4,8,20)){
@@ -173,7 +173,7 @@ function(){
     phatmat[,i] <- mleSim(2,p,R)[,1]
     i = i + 1;
   }
-  
+
   par(mfrow=c(2,2))
   i = 1
   for(R in c(2,4,8,20)){
@@ -181,11 +181,11 @@ function(){
     title(xlab=expression(bolditalic(p)),line=2)
     i = i + 1
   }
-  
+
 }
 
 function(){
-  
+
   p=1:100;p=p/sum(p);
   par(mfrow=c(2,2));
   par(mar=c(4,5.1,1.5,1.5))
@@ -196,7 +196,7 @@ function(){
     plot(p,phat[,1],main=paste("m = ",i, ", R = ",2, sep=""), ylab=expression(hat(bolditalic(p))),xlab="");abline(0,1)
     title(xlab=expression(bolditalic(p)),line=2)
   }
-  
+
 }
 
 function(){
@@ -233,7 +233,7 @@ function(){
 function(){
   p = 1:100; p = p/sum(p);d=length(p)
   R = 2;
-  
+
   lastvar=matrix(double(600*10),ncol=600);
   for(i in 1:10){
     (2^i)->m;
@@ -267,7 +267,7 @@ function(){
 function(){
   p = 1:100; p = p/sum(p);d=length(p)
   R = 2;
-  
+
   firstvar=matrix(double(600*10),ncol=600);
   for(i in 1:10){
     (2^i)->m;
@@ -289,7 +289,7 @@ function(){
   par(mfcol=c(2,2))
   par(mar=c(4,5.1,1.5,1.5))
   par(mgp=c(3.5,0.6,0))
-  
+
   firstvar=matrix(double(600*10),ncol=600);
   for(i in 1:10){
     (2^i)->m;
@@ -297,16 +297,16 @@ function(){
     firstvar[i,] = phat[1,]
     lastvar[i,] = phat[d,]
   }
-  
+
   #boxplot(t(lastvar),names=seq(2,20,2),main=expression(bold(paste("Decreasing var(",hat(italic(p))[100],") as ", italic(R)," increases",sep=""))),ylab=expression(hat(italic(p))[100]))
   boxplot(t(firstvar),names=(2^(1:10)),main=paste("R =",R),ylab=expression(hat(italic(p))[1]))
   title(xlab=expression(bold(m)),line=2)
-  
+
   #boxplot(t(lastvar),names=seq(2,20,2),main=expression(bold(paste("Decreasing var(",hat(italic(p))[100],") as ", italic(R)," increases",sep=""))),ylab=expression(hat(italic(p))[100]))
   boxplot(t(lastvar),names=(2^(1:10)),main=paste("R =",R),ylab=expression(hat(italic(p))[100]))
   title(xlab=expression(bold(m)),line=2)
-  
-  
+
+
   2->m;
   firstvar=matrix(double(600*10),ncol=600);
   for(i in 1:10){
@@ -320,7 +320,7 @@ function(){
   #boxplot(t(lastvar),names=seq(2,20,2),main=expression(bold(paste("Decreasing var(",hat(italic(p))[100],") as ", italic(R)," increases",sep=""))),ylab=expression(hat(italic(p))[100]))
   boxplot(t(lastvar),names=seq(2,20,2),main=paste("m =",m),ylab=expression(hat(italic(p))[100]))
   title(xlab=expression(bold(R)),line=2)
-  
+
 }
 
 function(){
@@ -336,7 +336,7 @@ function(){
       title(xlab=expression(bolditalic(p)),line=2)
     }
   }
-  
+
 }
 require(ggplot2)
 function(){
@@ -344,25 +344,25 @@ function(){
   a = c(1, rep(100,99))
   a[1] = a[1] + 1
   b = c(101, rep(200,49), c(rep(200,48),300))
-  De = matrix(double(100*99),nrow =100); 
+  De = matrix(double(100*99),nrow =100);
   for(i in 1:50){s = (i-1)*2+1; De[c(s,s+1),i]=1}
   for(i in 51:99){s = (i-51)*2+2; De[c(s,s+1),i]=1}
   De[100,99] = 1
   WeaverBayes(a,b,t(De),tol=1e-15, maxit=10000, iteration=T)->res
-  
+
   layout(matrix(c(1,1,1,2,2,3), 2, 3, byrow = TRUE))
   par(mar=c(2.4,2.7,2.4,1.5))
   par(mgp=c(1.5,0.4,0))
   plot(res$iter$lnLik,xlab="Iterations",ylab="Log-likelihood", xaxt='n', type='p', cex=0.3)
   axis(1, at=c(1,seq(500,6500,1000),7038))
-  
+
   plot(res$iter$lnLik[1:250],xlab="Iterations",ylab="Log-likelihood", type='p', cex=0.8, xaxt='n', ylim = c(-122724,-122723.4))
   axis(1, at=c(1,seq(50,250,50)))
-  
+
   plot(res$iter$lnLik[7000:7038],xlab="Iterations",ylab="Log-likelihood", type='p', cex=0.9, xaxt='n',yaxt='n', ylim = c(-122724,-122723.4))
   axis(1, at=c(0,seq(10,30,10),38), labels=c(7000,seq(7010,7030,10),7038))
   axis(2, at=c(-122723.8, -122723.6, -122723.4))
-  
+
 }
 
 function(){
@@ -372,19 +372,19 @@ function(){
   De = c(1,1,0)
   it = WeaverBas(a,b,t(De),iteration=T)
   par(mfrow=c(1,3))
-  
+
   plot(it$iter$path[1:5,1]*10,it$iter$path[1:5,2]*10,type='p',cex=1,pch=19, xlab=expression(x[1]),ylab=expression(x[2]))
   text(it$iter$path[1:5,1]*10,it$iter$path[1:5,2]*10, label=0:4,pos=c(4,1,2,3,4))
   points(4,4,pch=10,col="red",cex=3)
-  
+
   plot(it$iter$path[1:5,1]*10,it$iter$path[1:5,3]*10,type='p',cex=1,pch=19, xlab=expression(x[1]),ylab=expression(x[3]))
   text(it$iter$path[1:5,1]*10,it$iter$path[1:5,3]*10, label=0:4,pos=c(1,2,3,4,1))
   points(4,2,pch=10,col="red",cex=3)
-  
+
   plot(it$iter$path[1:5,2]*10,it$iter$path[1:5,3]*10,type='p',cex=1,pch=19, xlab=expression(x[2]),ylab=expression(x[3]))
   text(it$iter$path[1:5,2]*10,it$iter$path[1:5,3]*10, label=0:4,pos=c(1,2,3,4,1))
   points(4,2,pch=10,col="red",cex=3)
-  
+
 }
 
 
@@ -455,28 +455,28 @@ sim_concreteExampleFixedPartitionNoCompleteDataUnindentifiable <-
     sdphat = NULL
     covar = NULL
     covphat = NULL
-    
+
     p = c(0.1654,0.2024,0.1444,0.1532,0.2301,0.1046)
     #p = (1:6) / sum(1:6)
     #sample2 - marginal gender
     p2 = c(sum(p[1:3]),sum(p[4:6]))
     ssize2 = 40
-    
+
     #sample3 - marginal age
     p3 = c(p[1]+p[4],p[2]+p[5],p[3]+p[6])
     ssize3 = 40
-    
+
     #sample4 - conditional
     p4 = c(p[1],p[4])
     ssize4 = 100
-    
-    
+
+
     #sample5 - conditional on male
     p5 = p[4:6]
     ssize5 = 40
-    
+
     De = matrix(c(1,1,1,0,0,0,0,0,0,1,1,1,1,0,0,1,0,0,0,1,0,0,1,0,0,0,1,0,0,1),nrow=6)
-    
+
     cl <- makeSOCKcluster(rep("localhost",12))
     clusterExport(cl, list("p","p2","p3","p4","p5","ssize2","ssize3","ssize4","ssize5","De","Weaver","WeaverBas","WeaverBayes","WeaverGre","cl","oim","covMLE"),envir=environment())
     res = parLapply(cl, 1:n, function(x){
@@ -496,7 +496,7 @@ sim_concreteExampleFixedPartitionNoCompleteDataUnindentifiable <-
       list(phat=phat,sd=sd,covar=covar,chi2 = chi2)
     })
     stopCluster(cl)
-    
+
     res
   }
 
@@ -509,28 +509,28 @@ sim_concreteExampleFixedPartitionNoCompleteDataModif1 <-
     sdphat = NULL
     covar = NULL
     covphat = NULL
-    
+
     p = c(0.1654,0.2024,0.1444,0.1532,0.2301,0.1046)
     #p = (1:6) / sum(1:6)
     #sample2 - marginal gender
     p2 = c(sum(p[1:3]),sum(p[4:6]))
     ssize2 = 40
-    
+
     #sample3 - marginal age
     p3 = c(p[1]+p[4],p[2]+p[5],p[3]+p[6])
     ssize3 = 40
-    
+
     #sample4 - conditional
     p4 = c(p[1],p[4])
     ssize4 = 100
-    
-    
+
+
     #sample5 - conditional on male
     p5 = c(p[2]+p[6], p[3]+p[5])
     ssize5 = 40
     print(environment())
     De = matrix(c(c(1,1,1,0,0,0,0,0,0,1,1,1,1,0,0,1,0,0,0,1,0,0,1,0,0,0,1,0,0,1),c(0,1,0,0,0,1,0,0,1,0,1,0,0,1,1,0,1,1)),nrow=6)
-    
+
     cl <- makeSOCKcluster(rep("localhost",12))
     clusterExport(cl, list("p","p2","p3","p4","p5","ssize2","ssize3","ssize4","ssize5","De","Weaver","WeaverBas","WeaverBayes","WeaverGre","cl","oim","covMLE"),envir=environment())
     res = parLapply(cl, 1:n, function(x){
@@ -548,7 +548,7 @@ sim_concreteExampleFixedPartitionNoCompleteDataModif1 <-
       list(phat=phat,sd=sd,covar=covar,chi2 = chi2)
     })
     stopCluster(cl)
-    
+
     res
   }
 
@@ -562,39 +562,39 @@ sim_concreteExampleFixedPartitionNoCompleteDataModif2 <-
     sdphat = NULL
     covar = NULL
     covphat = NULL
-    
+
     p = c(0.1654,0.2024,0.1444,0.1532,0.2301,0.1046)
     #p = (1:6) / sum(1:6)
     #sample2 - marginal gender
     p2 = c(sum(p[1:3]),sum(p[4:6]))
     ssize2 = 40
-    
+
     #sample3 - marginal age
     p3 = c(p[1]+p[4],p[2]+p[5],p[3]+p[6])
     ssize3 = 40
-    
+
     #sample4 - conditional
     p4 = c(p[1],p[4])
     ssize4 = 100
-    
-    
+
+
     #sample5 - conditional on male
     p5 = c(p[2]+p[6], p[3]+p[5])
     ssize5 = 40
-    
+
     #sample6 - replace sample4
     p6 = c(p[1]+p[5], p[2]+p[4])
     ssize6 = 40
-    
+
     print(environment())
     De = matrix(c(c(1,1,1,0,0,0,0,0,0,1,1,1,1,0,0,1,0,0,0,1,0,0,1,0,0,0,1,0,0,1),c(0,1,0,0,0,1,0,0,1,0,1,0,0,1,1,0,1,1),c(1,0,0,0,1,0,0,1,0,1,0,0,1,1,0,1,1,0)),nrow=6)
-    
+
     cl <- makeSOCKcluster(rep("localhost",12))
     clusterExport(cl, list("p","p2","p3","p4","p5","p6","ssize2","ssize3","ssize4","ssize5","ssize6","De","Weaver","WeaverBas","WeaverBayes","WeaverGre","cl","oim","covMLE"),envir=environment())
     res = parLapply(cl, 1:n, function(x){
       x2 = as.vector(table(sample(x=c("123","456"),prob = p2, replace = T, size = ssize2)))
       x3 = as.vector(table(sample(x=c("14","25","36"),prob = p3, replace = T, size = ssize3)))
-      
+
       x5 = as.vector(table(sample(x=c("26","35"),prob = p5, replace = T, size = ssize5)))
       x6 = as.vector(table(sample(x=c("15","24"),prob = p6, replace = T, size = ssize6)))
       a = c(0,0,0,0,0,0)
@@ -607,7 +607,7 @@ sim_concreteExampleFixedPartitionNoCompleteDataModif2 <-
       list(phat=phat,sd=sd,covar=covar,chi2 = chi2)
     })
     stopCluster(cl)
-    
+
     res
   }
 
@@ -619,7 +619,7 @@ function(){
   phat = atrix(Reduce("c", sapply(y, function(x)x$phat)),nrow=6)
   sd = matrix(Reduce("c", sapply(y, function(x)x$sd)),nrow=6)
   covar=Reduce("+", sapply(y, function(x)x$covar))/n
-  
+
 }
 
 
